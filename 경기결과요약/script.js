ef4b6d9d5522721ff3197e0acda474de47ac42a7
jeonseonghyeok <39776515+jeonseonghyeok@users.noreply.github.com>
2023-07-13 12:08:28 +0900

    Update script.js
    승리명단 전송 방식 수정(텍스트 -> JSON)

--- 경기결과요약/script.js
+++ 경기결과요약/script.js
@@ -3,6 +3,9 @@ window.onload = function(){
   var now = new Date();
   document.getElementById('date').value = now.toISOString().substring(0, 10);
 }
+var oryuWinnerList;
+var gogaWinnerList;
+
 function GameResultDecomposition() {
   // 괄호 내 문자 포함한 경우 제거하고, VS(대소문자 구분 없이) 지우고, 2개 이상의 공백 1개로 변경하고, 줄바꿈제거 후, X경기 기준으로 분해
   //결과적으로 점수와 명단만 남음 ex)이용대 유연성 (20) 최솔규 김원호
@@ -39,37 +42,38 @@ function GameResultDecomposition() {
   });
 
   $("#winnerSortResult").val("오류(합산점수 : " + oryuGameTotalPoint + ")\n");
+  oryuWinnerList = ConversionToObject(oryuWinnerArr);
+  winResultPrint(oryuWinnerList);
 
-  winResultPrint(oryuWinnerArr);
   $("#winnerSortResult").val($("#winnerSortResult").val() + "고가(합산점수 : " + gogaGameTotalPoint + ")\n");
-  winResultPrint(gogaWinnerArr);
+  gogaWinnerList = ConversionToObject(gogaWinnerArr);
+  winResultPrint(gogaWinnerList);
+
   $("#saveForm").show();
 
 }
-function winResultPrint(winnerArr) {
-  winnerArr.sort();
-  var prevWinner = winnerArr[0];
-  var numberOfWins = 0;
-  var winResultPrintMessege = "";
-
-  winnerArr.forEach(e => {
-    if (e == prevWinner)
-      numberOfWins++;
-    else {
-      winResultPrintMessege += (prevWinner + " " + numberOfWins + "승\n");
-      prevWinner = e;
-      numberOfWins = 1;
-    }
+function ConversionToObject(array) {
+    array.sort();
+    return array.reduce((pv, cv)=>{
+        pv[cv] = (pv[cv] || 0) + 1;
+        return pv;
+    }, {});
+}
+function winResultPrint(winnerList) {
+  var returnMessege = "";
+  //const keyArr = Object.keys(winnerList);
+  Object.keys(winnerList).forEach(e => {
+    returnMessege += (e + " " + winnerList[e]+"승\n");
   });
-  if (prevWinner != undefined) {
-    winResultPrintMessege += (prevWinner + " " + numberOfWins + "승\n\n");
-    $("#winnerSortResult").val($("#winnerSortResult").val() + winResultPrintMessege);
-  }
+    returnMessege += "\n";
+  $("#winnerSortResult").val($("#winnerSortResult").val() + returnMessege);
+
 }
 
 function GameResultSave(){
   var gameTime = document.querySelector('input[id="date"]').value +" "+ document.querySelector('input[id="time"]').value;
-  var scriptLink = "AKfycbwcVocVpIPT_uFXu2DrnHabzEts1-9WSO2xGIH26xf2BXjFri2gHXkCSVsZGWh4lklt8A";
+  var scriptLink = "AKfycbxk--xhwahqnFFAM6pbXU1ydFpKaHARdyQa0Hhkn3yfeJ24RswReTuNRRFab3Ua_uWfgA";
+  var scriptLink_test = "AKfycbwyQLNDHtIn-aLH3z189SFItcX9B29021xm2zMVF8n9";
   var writer = localStorage.getItem("writer");
   if(writer == null){
     var inputName = prompt("작성자 성함을 기입해주세요.");
@@ -80,21 +84,21 @@ function GameResultSave(){
     else
       return 0;
   }
-  if(confirm("해당 출력을 기록하시겠습니까?(작성자 : "+writer+")")){
+  if(confirm("승리명단을 기록하시겠습니까?(작성자 : "+writer+")")){
     $.ajax({
       type: "get",
       data: {
         "gameTime" : gameTime,
         "writer" : writer,
-        "gameResult": $("#winnerSortResult").val()
+        "gameResult": JSON.stringify(Object.assign(oryuWinnerList,gogaWinnerList))
       },
       url: "https://script.google.com/macros/s/"+scriptLink+"/exec",
-      //url: "https://script.google.com/macros/s/"+scriptLink+"/dev",
+      // url: "https://script.google.com/macros/s/"+scriptLink_test+"/dev",
       success: function(response){
         console.log(response);
         if(response.result == "success")
           alert('입력 완료.');
-        else if((response.result == "error")) 
+        else if((response.result == "error"))
           alert(response.errorMessage);
       }
     });
