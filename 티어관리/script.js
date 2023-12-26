e8a86c72c571b9a7ef29e1f8d107923db97b4914
jeonseonghyeok <39776515+jeonseonghyeok@users.noreply.github.com>
2023-12-26 10:20:25 +0900

    티어 관리툴 변경(임시)
    명단리스트 업데이트 전이라서 불러오기(import) 주석처리, 하드코딩으로  주입

--- 티어관리/script.js
+++ 티어관리/script.js
@@ -1,23 +1,25 @@
 var memberInfo;
 window.onload = function() {
     memberInfoImport();
+    $("#teamTierManage").show();
 }
 
 function memberInfoImport() {
-    $.ajax({
-        type: "GET",
-        async: false, //동기방식으로 사용
-        datatype: "JSON",
-        //url:"../memberInfoList",
-        url: "https://jeonseonghyeok.github.io/SCV/memberInfoList",
-        success: function(result) {
-            memberInfo = JSON.parse(result);
-            $("#teamTierManage").show();
-        }
-    });
+	memberInfo = {"김준선":{"tier":6,"sex":1},"이현":{"tier":4},"이승재":{"tier":6,"sex":1},"김태수":{"tier":3},"김규혁":{"tier":6,"sex":1},"이성훈":{"tier":5,"sex":1},"박준의":{"tier":3},"김대원":{"tier":3,"sex":1},"정해준":{"tier":5,"sex":1},"최민선":{"tier":3,"sex":2},"노우석":{"tier":4,"sex":1},"서광철":{"tier":5,"sex":1},"최진영":{"tier":4,"sex":1},"박형기":{"tier":2,"sex":1},"장익석":{"tier":2,"sex":1},"고은비":{"tier":3,"sex":2},"이익제":{"tier":2,"sex":1},"김성권":{"tier":4,"sex":1},"선우윤호":{"tier":1,"sex":1},"정창수":{"tier":3,"sex":1},"최보경":{"tier":2},"윤효진":{"tier":2,"sex":2},"문정경":{"tier":2,"sex":2},"홍봄이":{"tier":1,"sex":2},"전성혁":{"tier":3,"sex":1},"김진":{"tier":4,"sex":1},"강선문":{"tier":4,"sex":1},"배종천":{"tier":3,"sex":1},"신재성":{"tier":5,"sex":1},"이정석":{"tier":4,"sex":1},"서승범":{"tier":2,"sex":1},"홍지수":{"tier":2,"sex":2},"이규현":{"tier":1,"sex":1},"방희연":{"tier":1,"sex":2},"박찬":{"tier":1,"sex":1},"김대원2":{"tier":1,"sex":1},"강지수":{"tier":2,"sex":2},"오종혁":{"tier":2,"sex":1},"이창빈":{"tier":3,"sex":1},"강래엽":{"tier":2,"sex":1},"송승범":{"tier":2,"sex":1},"류시원":{"tier":1},"박종찬":{"tier":3,"sex":1},"방한결":{"tier":5,"sex":2},"유연진":{"tier":1,"sex":1},"정상열":{"tier":4,"sex":1},"배현민":{"tier":1,"sex":1},"심예림":{"tier":4,"sex":2},"박주성":{"tier":1,"sex":1},"윤윤하":{"tier":4,"sex":2},"송지우":{"tier":4,"sex":1},"장철원":{"tier":1,"sex":1},"전나원":{"tier":1,"sex":2},"박우영":{"tier":2,"sex":1},"정우민":{"tier":1,"sex":1},"김남우":{"tier":5,"sex":2},"송인혁":{"tier":2,"sex":1},"최용진":{"tier":3,"sex":1},"이승채":{"sex":1,"tier":2},"이현재":{"sex":1,"tier":3}};
+//    $.ajax({
+//        type: "GET",
+//        async: false, //동기방식으로 사용
+//        datatype: "JSON",
+//        //url:"../memberInfoList",
+//        url: "https://jeonseonghyeok.github.io/SCV/memberInfoList",
+//        success: function(result) {
+//            memberInfo = JSON.parse(result);
+//            $("#teamTierManage").show();
+//        }
+//    });
 }
 
-function memeberSearch() {
+function memberSearch() {
     //명단 조회
     let searchName = prompt('이름을 입력하시오.');
     let choice;
@@ -26,116 +28,96 @@ function memeberSearch() {
     if (searchName == null || searchName == '')
         alert("미입력 또는 취소로 종료");
     else if (member != undefined) {
-        memeberInfoPrint(searchName);
-        choice = confirm("정보를 수정하시겠습니까?");
-        if (choice) {
-            memeberInfoChange(searchName) //팀 또는 티어를 변경
+        memberInfoPrint(searchName);
+        choice = confirm("티어를 수정하시겠습니까?");
+        if (choice && memberTierChange(searchName)) {//티어를 변경 완료
+        	memberInfoPrint(searchName);
         }
     } else {
         let confirmMessege = "";
         confirmMessege += "'" + searchName + "'은(는) 명단에 존재하지 않습니다.\n";
         confirmMessege += "등록하시겠습니까?";
         choice = confirm(confirmMessege);
-        if (choice)
-            memeberSignUp(searchName);
+        if (choice && memberSignUp(searchName))
+        	memberInfoPrint(searchName);
+        else
+        	alert("미입력 또는 취소로 종료");
     }
 }
 
-function memeberSignUp(name) {
+function memberSignUp(name) {
     let promptMessege;
     var inputString;
 
     memberInfo[name] = {};
-    //console.log(memeberTeamChange(name));
 
-    if (memeberTeamChange(name) && memeberTierChange(name)) {
-        //localStorage.setItem("memberInfo", JSON.stringify(memberInfo));
-        //console.log(memberInfo[name]);
-
+    if (memberSexChange(name) && memberTierChange(name)) {
         alert("완료되었습니다.");
         $("#memberInfoBox").val("");
-        memeberInfoPrint(name);
+        return 1;
     }
-
+    else{
+    	delete(memberInfo[name]);
+    	return 0;
+    }
 }
 
-function memeberInfoChange(name) {
-    if (confirm("팀을 변경하시겠습니까?")) {
-        if (memeberTeamChange(name)) {
-            // localStorage.setItem("memberInfo", JSON.stringify(memberInfo));
-            alert("완료되었습니다.");
-        } else {
-            return 0;
-        }
-    } else if (confirm("티어를 변경하시겠습니까?")) {
-        if (memeberTierChange(name)) {
-            // localStorage.setItem("memberInfo", JSON.stringify(memberInfo));
-            alert("완료되었습니다.");
-        } else {
-            return 0;
-        }
-    } else {
-        alert("변경사항 없이 종료합니다.");
-        return 0;
-    }
-    $("#memberInfoBox").val("");
-    memeberInfoPrint(name);
-}
 
-function memeberTeamChange(name) {
+function memberSexChange(name) {
     let promptMessege;
     var inputString;
 
-    promptMessege = '팀을 입력하시오.\n';
-    promptMessege += '(오류:0 / 고가:1 / 깍두기:2 / 탈퇴:3)';
+    promptMessege = '성별을 입력하시오.\n';
+    promptMessege += '(남성:1 / 여성:2)';
     inputString = prompt(promptMessege, '');
     if (inputString == null || inputString == "") {
-        alert("미입력 또는 취소로 종료");
         return 0;
-    } else if (inputString == 3) {
-        if (confirm('명단에서 제거하시겠습니까?', '')) {
-            delete(memberInfo[name]);
-            alert('제거 완료');
-        } else {
-            alert('취소');
-            return 0;
-        }
+    } 
+    else {
+    	memberInfo[name].sex = Number(inputString);
+        return 1;
     }
-    memberInfo[name].team = Number(inputString);
-    return 1;
+    
+    
 }
 
-function memeberTierChange(name) {
-    promptMessege = '티어를 입력하시오.(1~5)\n';
-    promptMessege += '(브 : 1/ 실 : 2/ 골 : 3/ 에 : 4)';
+function memberTierChange(name) {
+    promptMessege = '티어를 입력하시오.(1~6)\n';
+    promptMessege += ' - 브론즈 : 1 / 실버 : 2 / 골드 : 3\n - 플레티넘 : 4 / 에메랄드 : 5 / 마스터 : 6';
     inputString = prompt(promptMessege, '');
     //console.log(inputString);
     if (inputString == null || inputString == "") {
-        alert("미입력 또는 취소로 종료");
         return 0;
     }
-    memberInfo[name].tier = Number(inputString);
+    else{
+    	var inputTierLever = Number(inputString);
+    	if(inputTierLever == -1){
+    		delete(memberInfo[name]);
+    		alert("명단 삭제 완료");
+            $("#memberInfoBox").val("");
+    	}
+    	else if(inputTierLever >= 0 && inputTierLever <=6)
+    		memberInfo[name].tier = inputTierLever;
+    	else
+    		return 0;
+    }
     return 1;
 }
 
-function memeberInfoPrint(searchName) {
+function memberInfoPrint(searchName) {
 
     let playerInfo = "";
     const member = memberInfo[searchName];
 
     playerInfo += "[회원 정보]\n";
     playerInfo += "이름 : " + searchName + "\n";
-    playerInfo += "팀 : ";
-    //console.log(member.team);
-    switch (member.team) {
-        case 0:
-            playerInfo += '오류';
-            break;
+    playerInfo += "성별 : ";
+    switch (member.sex) {
         case 1:
-            playerInfo += '고가';
+            playerInfo += '남성';
             break;
         case 2:
-            playerInfo += '깍두기';
+            playerInfo += '여성';
             break;
         default:
             playerInfo += '누락';
@@ -154,7 +136,13 @@ function memeberInfoPrint(searchName) {
             playerInfo += '골드';
             break;
         case 4:
-            playerInfo += '에메랄드';
+            playerInfo += '플레티넘';
+            break;
+        case 5:
+            playerInfo += '마스터';
+            break;
+        case 6:
+            playerInfo += '다이아';
             break;
         default:
             playerInfo += '미정';
@@ -163,16 +151,36 @@ function memeberInfoPrint(searchName) {
     alert(playerInfo);
 }
 
-function memebersManage() {
-    if (confirm("팀을 일괄적으로 변경하시겠습니까?")) {
-        if (memeberListTeamChange()) {
-            // localStorage.setItem("memberInfo", JSON.stringify(memberInfo));
-            alert("완료되었습니다.");
-            $("#memberInfoBox").val("");
+function memberListPrintOrderByTier(){
+	// 3행 6열의 배열 생성 및 초기화
+	var memberArr = Array.from({ length: 3 }, () => Array.from({ length: 7 }, () => []));
+	const tierName = ['🏸','Bronze','Silver','Gold','Platinum','Diamond','Master'];
+	const sexName = ['','남자','여자'];
+
+    for (var key in memberInfo) {
+        if (!memberInfo[key].hasOwnProperty("sex") || !memberInfo[key].hasOwnProperty("tier")) { // 성별이 없는거나 티어가 없는 경우 누락으로
+            memberArr[0][0].push(key);
+        } else {
+            memberArr[memberInfo[key].sex][memberInfo[key].tier].push(key);
         }
-    } else if (confirm("티어를 일괄적으로 변경하시겠습니까?")) {
-        if (memeberListTierChange()) {
-            // localStorage.setItem("memberInfo", JSON.stringify(memberInfo));
+    }
+	var printContent = "";
+    // 배열 순환하면서 출력
+    for (var i = 1; i < memberArr.length; i++) { //남,여 순서
+    	 printContent += (sexName[i] + ' \n');
+	      for (var j = memberArr[i].length-1; j > 0; j--) {
+	    	  printContent += (tierName[j]+'('+j+')' + '\n');
+	    	  printContent += (memberArr[i][j].join(' / ')+'\n\n');
+	      }
+	      printContent += '─────────────────────────\n\n';
+    }
+    printContent += ('누락 : ' + memberArr[0][0]);
+    $("#memberInfoBox").val(printContent);
+}
+
+function membersManage() {
+    if (confirm("티어를 일괄적으로 변경하시겠습니까?")) {
+        if (memberListTierChange()) {
             alert("완료되었습니다.");
             $("#memberInfoBox").val("");
         }
@@ -180,7 +188,7 @@ function memebersManage() {
         alert("변경 사항 없이 종료합니다.");
 }
 
-function memeberListTierChange() {
+function memberListTierChange() {
     let list = prompt('명단을 입력하시오.');
     var inputString;
 
@@ -188,30 +196,41 @@ function memeberListTierChange() {
         alert("명단 입력이 필요합니다.");
         return 0;
     } else {
-        promptMessege = '티어를 입력하시오.(1~5)\n';
-        promptMessege += '(브 : 1/ 실 : 2/ 골 : 3/ 에 : 4)';
-        inputString = Number(prompt(promptMessege, ''));
-
+        promptMessege = '티어를 입력하시오.(1~6)\n';
+        promptMessege += ' - 브론즈 : 1 / 실버 : 2 / 골드 : 3\n - 플레티넘 : 4 / 에메랄드 : 5 / 마스터 : 6';
+        inputString = prompt(promptMessege, '');
+        //console.log(inputString);
         if (inputString == null || inputString == "") {
-            alert("미입력 또는 취소로 종료");
             return 0;
-        } else {
+        }
+        else{
+        	var inputTierLever = Number(inputString);
             const players = list.replace("  ", " ").split(" ");
-
-            for (var i = 0; i < players.length; i++) {
-                if (memberInfo[players[i]] == undefined)
-                    alert("'" + players[i] + "'은(는) 명단에 존재하지 않아 미처리됩니다.");
-                else {
-                    console.log(players[i] + " : " + memberInfo[players[i]].tier + "->" + inputString);
-                    memberInfo[players[i]].tier = inputString;
+        	if(inputTierLever == -1){
+        		for (var i = 0; i < players.length; i++) {
+        			delete(memberInfo[players[i]]);
                 }
-            }
-            return 1;
+        		alert("명단 삭제 완료");
+        		return 1;
+        	}
+        	else if(inputTierLever >= 0 && inputTierLever <=6){
+        		for (var i = 0; i < players.length; i++) {
+                    if (memberInfo[players[i]] == undefined)
+                        alert("'" + players[i] + "'은(는) 명단에 존재하지 않아 미처리됩니다.");
+                    else {
+                        console.log(players[i] + " : " + memberInfo[players[i]].tier + "->" + inputString);
+                        memberInfo[players[i]].tier = inputTierLever;
+                    }
+                }
+        		return 1;
+        	}
+        	else
+        		return 0;
         }
     }
 }
 
-function memeberListTeamChange() {
+function memberListTeamChange() {
     let list = prompt('명단을 입력하시오.');
     var inputString;
 
@@ -255,11 +274,11 @@ function memeberListTeamChange() {
 
 }
 
-function memeberInfoPrintByJson() {
+function memberListPrintByJson() {
     $("#memberInfoBox").val(JSON.stringify(memberInfo));
 }
 
-function memebersTeamReset() {
+/*function membersTeamReset() {
     if (confirm("팀을 초기화 하시겠습니까?")) {
         for (var key in memberInfo) {
             if (memberInfo.hasOwnProperty(key)) { // 객체의 고유한 속성만 처리
@@ -267,63 +286,8 @@ function memebersTeamReset() {
             }
         }
     }
-}
+}*/
 
-function memeberNameByTeamOrder() {
-    var memberArr = [
-        [],
-        [],
-        [],
-        []
-    ]; //4개 생성
-
-    for (var key in memberInfo) {
-        if (memberInfo[key].hasOwnProperty("team")) { // team이 있을 경우에만 처리 0,1,2 이외에는 없을 것으로 간주
-            memberArr[memberInfo[key].team].push(key);
-        } else {
-            memberArr[3].push(key);
-        }
-    }
-    //출력	
-    memberNamePrint(memberArr);
-}
-
-function removeTeamMissingMembers() {
-    if (confirm("팀 누락명단에 있는 멤버를 제거하시겠습니까?")) {
-        for (var key in memberInfo) {
-            if (!memberInfo[key].hasOwnProperty('team')) {
-                delete(memberInfo[key]);
-            }
-        }
-    }
-
-}
-
-function memberNamePrint(memberArr) {
-    let printMessege = "";
-    for (var i = 0; i < memberArr.length; i++) {
-        switch (i) {
-            case 0:
-                printMessege += '오류';
-                break;
-            case 1:
-                printMessege += '고가';
-                break;
-            case 2:
-                printMessege += '깍두기';
-                break;
-            default:
-                printMessege += '누락';
-        }
-        printMessege += ("(" + memberArr[i].length + ")");
-        printMessege += '\n';
-        memberArr[i].sort().forEach(function(memberName) {
-            printMessege += (memberName + "\n");
-        });
-        printMessege += '\n\n';
-    };
-    $("#memberInfoBox").val(printMessege);
-}
 
 function ConversionToObject(array) {
     array.sort();
@@ -332,3 +296,25 @@ function ConversionToObject(array) {
         return pv;
     }, {});
 }
+
+/**
+ * 성별 일관 기입 또는 변경
+let memberlist = prompt('명단 입력');
+let promptMessege = '성별을 입력하시오.\n';
+promptMessege += '(남성:1 / 여성:2)';
+let inputSex = Number(prompt(promptMessege, ''));
+const players = memberlist.replace("  ", " ").split(" ");
+var successResult = "";
+
+for (var i = 0; i < players.length; i++) {
+	if (memberInfo[players[i]] == undefined)
+		console.log("'" + players[i] + "'은(는) 명단에 존재하지 않아 미처리");
+	else {
+		memberInfo[players[i]].sex = inputSex;
+        console.log(memberInfo[players[i]]);
+    }
+}
+
+ */
+
+
