fe6d73a343bfe547ee3ef49fb4286bd2314dd25c
jeonseonghyeok <39776515+jeonseonghyeok@users.noreply.github.com>
2024-01-02 10:50:54 +0900

    Update script.js

--- 티어관리/script.js
+++ 티어관리/script.js
@@ -1,11 +1,11 @@
 var memberInfo;
 window.onload = function() {
     memberInfoImport();
+    $("#teamTierManage").show();
 }
 
 function memberInfoImport() {
-//memberInfo = {"김준선":{"tier":6,"sex":1},"이현":{"tier":4},"이승재":{"tier":6,"sex":1},"김규혁":{"tier":6,"sex":1},"이성훈":{"tier":5,"sex":1},"김대원":{"tier":3,"sex":1},"정해준":{"tier":5,"sex":1},"최민선":{"tier":3,"sex":2},"노우석":{"tier":3,"sex":1},"서광철":{"tier":5,"sex":1},"최진영":{"tier":4,"sex":1},"박형기":{"tier":2,"sex":1},"장익석":{"tier":3,"sex":1},"고은비":{"tier":3,"sex":2},"이익제":{"tier":2,"sex":1},"김성권":{"tier":4,"sex":1},"선우윤호":{"tier":1,"sex":1},"정창수":{"tier":2,"sex":1},"최보경":{"tier":2},"윤효진":{"tier":2,"sex":2},"문정경":{"tier":2,"sex":2},"홍봄이":{"tier":1,"sex":2},"전성혁":{"tier":3,"sex":1},"김진":{"tier":4,"sex":1},"강선문":{"tier":4,"sex":1},"배종천":{"tier":3,"sex":1},"신재성":{"tier":5,"sex":1},"이정석":{"tier":4,"sex":1},"서승범":{"tier":2,"sex":1},"홍지수":{"tier":2,"sex":2},"이규현":{"tier":1,"sex":1},"방희연":{"tier":1,"sex":2},"박찬":{"tier":1,"sex":1},"김대원2":{"tier":1,"sex":1},"강지수":{"tier":2,"sex":2},"오종혁":{"tier":2,"sex":1},"이창빈":{"tier":3,"sex":1},"강래엽":{"tier":2,"sex":1},"송승범":{"tier":2,"sex":1},"박종찬":{"tier":3,"sex":1},"방한결":{"tier":5,"sex":2},"유연진":{"tier":1,"sex":1},"정상열":{"tier":4,"sex":1},"배현민":{"tier":1,"sex":1},"심예림":{"tier":4,"sex":2},"박주성":{"tier":1,"sex":1},"윤윤하":{"tier":4,"sex":2},"송지우":{"tier":4,"sex":1},"장철원":{"tier":1,"sex":1},"전나원":{"tier":1,"sex":2},"박우영":{"tier":2,"sex":1},"정우민":{"tier":1,"sex":1},"김남우":{"tier":5,"sex":2},"송인혁":{"tier":3,"sex":1},"최용진":{"tier":3,"sex":1},"이현재":{"sex":1,"tier":3},"박준의":{"sex":1,"tier":3}};
-//$("#teamTierManage").show();
+//	memberInfo = {"김준선":{"tier":6,"sex":1},"이현":{"tier":4},"이승재":{"tier":6,"sex":1},"김규혁":{"tier":6,"sex":1},"이성훈":{"tier":5,"sex":1},"김대원":{"tier":3,"sex":1},"정해준":{"tier":5,"sex":1},"최민선":{"tier":3,"sex":2},"노우석":{"tier":3,"sex":1},"서광철":{"tier":5,"sex":1},"최진영":{"tier":4,"sex":1},"박형기":{"tier":2,"sex":1},"장익석":{"tier":3,"sex":1},"고은비":{"tier":3,"sex":2},"이익제":{"tier":2,"sex":1},"김성권":{"tier":4,"sex":1},"선우윤호":{"tier":1,"sex":1},"정창수":{"tier":2,"sex":1},"최보경":{"tier":2},"윤효진":{"tier":2,"sex":2},"문정경":{"tier":2,"sex":2},"홍봄이":{"tier":1,"sex":2},"전성혁":{"tier":3,"sex":1},"김진":{"tier":4,"sex":1},"강선문":{"tier":4,"sex":1},"배종천":{"tier":3,"sex":1},"신재성":{"tier":5,"sex":1},"이정석":{"tier":4,"sex":1},"서승범":{"tier":2,"sex":1},"홍지수":{"tier":2,"sex":2},"이규현":{"tier":1,"sex":1},"방희연":{"tier":1,"sex":2},"박찬":{"tier":1,"sex":1},"김대원2":{"tier":1,"sex":1},"강지수":{"tier":2,"sex":2},"오종혁":{"tier":2,"sex":1},"이창빈":{"tier":3,"sex":1},"강래엽":{"tier":2,"sex":1},"송승범":{"tier":2,"sex":1},"박종찬":{"tier":3,"sex":1},"방한결":{"tier":5,"sex":2},"유연진":{"tier":1,"sex":1},"정상열":{"tier":4,"sex":1},"배현민":{"tier":1,"sex":1},"심예림":{"tier":4,"sex":2},"박주성":{"tier":1,"sex":1},"윤윤하":{"tier":4,"sex":2},"송지우":{"tier":4,"sex":1},"장철원":{"tier":1,"sex":1},"전나원":{"tier":1,"sex":2},"박우영":{"tier":2,"sex":1},"정우민":{"tier":1,"sex":1},"김남우":{"tier":5,"sex":2},"송인혁":{"tier":3,"sex":1},"최용진":{"tier":3,"sex":1},"이현재":{"sex":1,"tier":3},"박준의":{"sex":1,"tier":3}};
     $.ajax({
         type: "GET",
         async: false, //동기방식으로 사용
@@ -151,32 +151,6 @@ function memberInfoPrint(searchName) {
     alert(playerInfo);
 }
 
-function memberListPrintOrderByTier(){
-	// 3행 6열의 배열 생성 및 초기화
-	var memberArr = Array.from({ length: 3 }, () => Array.from({ length: 7 }, () => []));
-	const tierName = ['🏸','Bronze','Silver','Gold','Platinum','Diamond','Master'];
-	const sexName = ['','남자','여자'];
-
-    for (var key in memberInfo) {
-        if (!memberInfo[key].hasOwnProperty("sex") || !memberInfo[key].hasOwnProperty("tier")) { // 성별이 없는거나 티어가 없는 경우 누락으로
-            memberArr[0][0].push(key);
-        } else {
-            memberArr[memberInfo[key].sex][memberInfo[key].tier].push(key);
-        }
-    }
-	var printContent = "";
-    // 배열 순환하면서 출력
-    for (var i = 1; i < memberArr.length; i++) { //남,여 순서
-    	 printContent += (sexName[i] + ' \n');
-	      for (var j = memberArr[i].length-1; j > 0; j--) {
-	    	  printContent += (tierName[j]+'('+j+')' + '\n');
-	    	  printContent += (memberArr[i][j].join(' / ')+'\n\n');
-	      }
-	      printContent += '─────────────────────\n\n';
-    }
-    printContent += ('누락 : ' + memberArr[0][0]);
-    $("#memberInfoBox").val(printContent);
-}
 
 function membersManage() {
     if (confirm("티어를 일괄적으로 변경하시겠습니까?")) {
@@ -274,9 +248,50 @@ function memberListTeamChange() {
 
 }
 
-function memberListPrintByJson() {
+function memberListPrintToJson() {
     $("#memberInfoBox").val(JSON.stringify(memberInfo));
 }
+
+function memberListPrintOrderByTier(){
+	// 3행 6열의 배열 생성 및 초기화
+	var memberArr = Array.from({ length: 3 }, () => Array.from({ length: 7 }, () => []));
+	const tierName = ['🏸','Bronze','Silver','Gold','Platinum','Diamond','Master'];
+	const sexName = ['','남자','여자'];
+
+    for (var key in memberInfo) {
+        if (!memberInfo[key].hasOwnProperty("sex") || !memberInfo[key].hasOwnProperty("tier")) { // 성별이 없는거나 티어가 없는 경우 누락으로
+            memberArr[0][0].push(key);
+        } else {
+            memberArr[memberInfo[key].sex][memberInfo[key].tier].push(key);
+        }
+    }
+	var printContent = "";
+    // 배열 순환하면서 출력
+    for (var i = 1; i < memberArr.length; i++) { //남,여 순서
+    	 printContent += (sexName[i] + ' \n');
+	      for (var j = memberArr[i].length-1; j > 0; j--) {
+	    	  printContent += (tierName[j]+'('+j+')' + '\n');
+	    	  printContent += (memberArr[i][j].sort().join(' / ')+'\n\n');
+	      }
+	      printContent += '─────────────────────\n\n';
+    }
+    printContent += ('누락 : ' + memberArr[0][0]);
+    $("#memberInfoBox").val(printContent);
+}
+
+function memberListPrintOrderByName() {
+	// 객체의 키를 정렬한 배열 생성
+    $("#memberInfoBox").val(Object.keys(memberInfo).sort().join('\n'));
+}
+
+/*function ConversionToObject(array) {
+    array.sort();
+    return array.reduce((pv, cv) => {
+        pv[cv] = (pv[cv] || 0) + 1;
+        return pv;
+    }, {});
+}*/
+
 
 /*function membersTeamReset() {
     if (confirm("팀을 초기화 하시겠습니까?")) {
@@ -287,15 +302,6 @@ function memberListPrintByJson() {
         }
     }
 }*/
-
-
-function ConversionToObject(array) {
-    array.sort();
-    return array.reduce((pv, cv) => {
-        pv[cv] = (pv[cv] || 0) + 1;
-        return pv;
-    }, {});
-}
 
 /**
  * 성별 일관 기입 또는 변경
